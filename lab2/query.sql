7e33f3a62a1e4426235dc8fe4a346733e66a8d82
Bebren305 <97741447+Daurennnn@users.noreply.github.com>
2026-09-21 22:45:30 +0500

    renames and constraints
    Renamed tables and columns, added foreign key constraints.

--- lab2/query.sql
+++ lab2/query.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE Airline_info (
 	airline_id SERIAL PRIMARY KEY NOT NULL,
 	airline_code VARCHAR(30) NOT NULL,
@@ -37,7 +35,7 @@ CREATE TABLE Baggage (
 	booking_id INTEGER NOT NULL
 );
 
-CREATE TABLE Bording_pass (
+CREATE TABLE Boarding_pass (
 	boarding_pass_id SERIAL PRIMARY KEY NOT NULL,
 	booking_id INTEGER NOT NULL,
 	seat VARCHAR(50) NOT NULL,
@@ -57,6 +55,7 @@ CREATE TABLE Booking_flight (
 CREATE TABLE booking (
 	booking_id SERIAL PRIMARY KEY NOT NULL,
 	flight_id INTEGER NOT NULL,
+	passenger_id INTEGER NOT NULL,
 	booking_platform VARCHAR(50) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP NOT NULL,
@@ -102,3 +101,60 @@ CREATE TABLE Security_check (
 );
 
 
+ALTER TABLE Airline_info
+RENAME TO Airline;
+
+ALTER TABLE booking
+RENAME COLUMN price TO ticket_price ;
+
+ALTER TABLE flights
+ALTER COLUMN departing_gate TYPE TEXT;
+
+
+ALTER TABLE Security_check
+ADD CONSTRAINT fk_security_check_passenger
+FOREIGN KEY (passenger_id) REFERENCES Passengers (passenger_id);
+
+ALTER TABLE booking
+ADD CONSTRAINT fk_booking_passenger
+FOREIGN KEY (passenger_id) REFERENCES Passengers (passenger_id);
+
+ALTER TABLE Baggage_check
+ADD CONSTRAINT fk_baggage_check_passenger
+FOREIGN KEY (passenger_id) REFERENCES Passengers (passenger_id);
+
+
+ALTER TABLE Baggage_check
+ADD CONSTRAINT fk_baggage_check_booking
+FOREIGN KEY (booking_id) REFERENCES booking (booking_id);
+
+ALTER TABLE Baggage
+ADD CONSTRAINT fk_baggage_booking
+FOREIGN KEY (booking_id) REFERENCES booking (booking_id);
+
+ALTER TABLE Boarding_pass
+ADD CONSTRAINT fk_boarding_pass_booking
+FOREIGN KEY (booking_id) REFERENCES booking (booking_id);
+
+ALTER TABLE Booking_flight
+ADD CONSTRAINT fk_booking_flight_booking
+FOREIGN KEY (booking_id) REFERENCES booking (booking_id);
+
+
+ALTER TABLE Booking_flight
+ADD CONSTRAINT fk_booking_flight_flight
+FOREIGN KEY (flight_id) REFERENCES flights (flight_id);
+
+
+ALTER TABLE flights
+ADD CONSTRAINT fk_flights_departing_airport
+FOREIGN KEY (departing_airport_id) REFERENCES Airport (airport_id);
+
+ALTER TABLE flights
+ADD CONSTRAINT fk_flights_arriving_airport
+FOREIGN KEY (arriving_airport_id) REFERENCES Airport (airport_id);
+
+
+ALTER TABLE flights
+ADD CONSTRAINT fk_flights_airline
+FOREIGN KEY (airline_id) REFERENCES Airline (airline_id);
